16e8ba9e1074670c180a006ac6bc7178ed5141dc
CarlosDanieldias <cjustinodias@gmail.com>
2024-11-14 20:07:17 -0300

    PROJETO DE CARTAS ALURINHA

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma string?',
+    'Sequência de caracteres'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz planta em Inglês?',
+    'Planta em ingles é plant'
+)
